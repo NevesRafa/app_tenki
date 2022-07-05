5c44bb721321567b47c217574440a64bbe5a1035
Rafael Neves <78914355+NevesRafa@users.noreply.github.com>
2022-07-05 15:31:10 -0300

    Created home screen layout.

--- app/src/main/java/com/nevesrafael/tenki/HomeScreenActivity.kt
+++ app/src/main/java/com/nevesrafael/tenki/HomeScreenActivity.kt
@@ -3,9 +3,9 @@ package com.nevesrafael.tenki
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class HomeScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home_screen)
     }
 }
